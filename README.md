ae74ab1c35a4d6ec2bcd190c8c286ddbf3a28f14
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-03-14 08:59:16 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # feathered-boa
- Python in a feathered boa.  Small items that make you look marvelous.
+
+Python in a feathered boa. Small items that make you look marvelous.
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def search_files(directory='.', extension=''):
+    extension = extension.lower()
+    for dirpath, dirnames, files in os.walk(directory):
+        for name in files:
+            if extension and name.lower().endswith(extension):
+                print(os.path.join(dirpath, name))
+            elif not extension:
+                print(os.path.join(dirpath, name))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+search_files('.', 'py')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# .idea
+# examples
+# inputs
+# outputs
+# programs
+# sketchpad
+# .gitattributes
+# .gitignore
+# findBizName.py
+# main.py
+# phoneAndemail.pyw
+# README.md
